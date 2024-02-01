268e2ea04b3dd839d35544206ea69fb906a9e550
SquirrelloDev <olejnike2000@gmail.com>
2024-02-01 18:38:44 +0100

    Add welcome page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@mantine/core/styles.css'
 import './sass/main.scss'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {appRoutes} from "./utils/routes";
@@ -14,6 +15,7 @@ import {QueryClientProvider} from "@tanstack/react-query";
 import {queryClient} from "./utils/api";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import {MantineProvider} from "@mantine/core";
 
 const router = createBrowserRouter([
     {
@@ -41,7 +43,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
+            <MantineProvider>
             <RouterProvider router={router}/>
+            </MantineProvider>
         </QueryClientProvider>
     </React.StrictMode>,
 )
