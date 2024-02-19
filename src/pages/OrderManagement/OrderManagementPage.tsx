791ee7f7014029487063efda03d47e81eef73c3b
SquirrelloDev <olejnike2000@gmail.com>
2024-02-19 13:34:48 +0100

    Add missing key prop

--- src/pages/OrderManagement/OrderManagementPage.tsx
+++ src/pages/OrderManagement/OrderManagementPage.tsx
@@ -17,13 +17,12 @@ function OrderManagementPage() {
 	}, [])
 	const userData = useAuthStore(state => state.userData)
 	const {data, isLoading, isSuccess, error, isError} = useUserOrdersQuery({id: userData.id, token: userData.token})
-	console.log("Render OrderPage")
 	return (
 		<section className={classes.management}>
 			<h1>Moje diety</h1>
 			<div className={classes.management__container}>
 				{isLoading && <Grid/>}
-				{!isLoading && data?.orders.map((order, idx) => <OrderEntry name={order.name} dietType={order.diet_id.diet_type} dietName={order.diet_id.name} calories={order.calories} subDate={order.sub_date} address={order.address_id} index={idx} openedItem={openedItem} setOpenedItem={selectItem} />)}
+				{!isLoading && data?.orders.map((order, idx) => <OrderEntry order={order} index={idx} openedItem={openedItem} setOpenedItem={selectItem} key={order._id}/>)}
 			</div>
 		</section>
 	)
