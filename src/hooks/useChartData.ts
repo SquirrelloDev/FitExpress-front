f3b483c02a729202d22df77aa432818462dea409
SquirrelloDev <olejnike2000@gmail.com>
2024-02-22 13:50:51 +0100

    Refactor hook to use single value

---
+++ src/hooks/useChartData.ts
@@ -0,0 +1,14 @@
+import {useMemo} from "react";
+
+function useChartData(data: {date: Date, water?: number, weight? :number}[]) {
+    const chartData = useMemo(() => {
+        return data.filter((item, idx) => idx < 7).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).map(datum => (
+            {
+                date: new Date(datum.date).toLocaleDateString(),
+                ...(datum.water && { water: (datum.water / 1000).toFixed(2)}),
+                ...(datum.weight && { weight: datum.weight}),
+            }))
+    }, [data])
+    return chartData
+}
+export default useChartData
--- src/hooks/useWaterChart.ts
+++
@@ -1,9 +0,0 @@
-import {useMemo} from "react";
-
-function useWaterChart(waterData: {date: Date, water: number}[]) {
-    const waterChartData = useMemo(() => {
-        return waterData.filter((waterItem, idx) => idx < 7).sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()).map(waterDatum => ({date: new Date(waterDatum.date).toLocaleDateString(), water: (waterDatum.water / 1000).toFixed(2)}))
-    }, [waterData])
-    return waterChartData
-}
-export default useWaterChart
