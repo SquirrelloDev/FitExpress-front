b45c2d146f4352d382b37d45c05303a03f4fb8c1
SquirrelloDev <olejnike2000@gmail.com>
2024-02-22 15:23:06 +0100

    Delete redundant component and replace it with new one

--- src/components/EntryDeleteSheet/DeleteSheet.tsx
+++ src/components/EntryDeleteSheet/DeleteSheet.tsx
@@ -1,15 +1,17 @@
-import useProgressDelete from "../../../queries/progress-entry/delete";
-import {queryClient} from "../../../utils/api";
+import useProgressDelete from "../../queries/progress-entry/delete";
+import {queryClient} from "../../utils/api";
 import {TailSpin} from "react-loader-spinner";
-import btnStyles from '../../../sass/components/button.module.scss'
+import btnStyles from '../../sass/components/button.module.scss'
 import clsx from "clsx";
-interface WaterDeleteSheetProps {
+import classes from "../../sass/components/entry-sheet.module.scss";
+interface DeleteSheetProps {
 	close: () => void,
 	userId: string
 	token: string
-	date: Date
+	date: Date,
+	kind: "water" | "weight"
 }
-function WaterDeleteSheet({close, userId, token, date}:WaterDeleteSheetProps) {
+function DeleteSheet({close, userId, token, date, kind}:DeleteSheetProps) {
 	const {mutate, isLoading, error} = useProgressDelete(() => {
 		queryClient.invalidateQueries(['List-Progress'])
 		close()
@@ -18,16 +20,18 @@ function WaterDeleteSheet({close, userId, token, date}:WaterDeleteSheetProps) {
 		mutate({
 			id: userId,
 			token: token,
-			kind: "water",
+			kind: kind,
 			date: date
 		})
 	}
 	return (
-		<div>
+		<div className={classes.delete}>
 			<h3>Czy na pewno usunąć wpis?</h3>
+			<div className={classes.delete__buttons}>
 			<button onClick={deleteEntry} className={clsx(btnStyles.btn, btnStyles['btn--danger'])}>{isLoading ? <TailSpin/> : 'Usuń'}</button>
 			<button onClick={close} disabled={isLoading} className={clsx(btnStyles.btn, btnStyles['btn--outline'])}>Anuluj</button>
+			</div>
 		</div>
 	)
 }
-export default WaterDeleteSheet
+export default DeleteSheet
