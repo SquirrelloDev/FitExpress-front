b556c38c381afd952687b62ce93a1c49735499a5
SquirrelloDev <olejnike2000@gmail.com>
2024-02-27 09:20:59 +0100

    Add deleting address

---
+++ src/components/AddressDeleteSheet/AddressDeleteSheet.tsx
@@ -0,0 +1,36 @@
+import useProgressDelete from "../../queries/progress-entry/delete";
+import {queryClient} from "../../utils/api";
+import {TailSpin} from "react-loader-spinner";
+import btnStyles from '../../sass/components/button.module.scss'
+import clsx from "clsx";
+import classes from "../../sass/components/entry-sheet.module.scss";
+import useAddressDelete from "../../queries/addresses/delete";
+import {toast} from "react-hot-toast";
+interface AddressDeleteSheetProps {
+	close: () => void,
+	addressId: string
+	token: string
+}
+function AddressDeleteSheet({close, addressId, token}:AddressDeleteSheetProps) {
+	const {mutate, isLoading} = useAddressDelete(() => {
+		toast.success('Adres pomyślnie usunięty!')
+		queryClient.invalidateQueries(['AddressesList'])
+		close()
+	})
+	const deleteEntry = () => {
+		mutate({
+			id: addressId,
+			token
+		})
+	}
+	return (
+		<div className={classes.delete}>
+			<h3>Czy na pewno usunąć adres?</h3>
+			<div className={classes.delete__buttons}>
+			<button onClick={deleteEntry} className={clsx(btnStyles.btn, btnStyles['btn--danger'])}>{isLoading ? <TailSpin/> : 'Usuń'}</button>
+			<button onClick={close} disabled={isLoading} className={clsx(btnStyles.btn, btnStyles['btn--outline'])}>Anuluj</button>
+			</div>
+		</div>
+	)
+}
+export default AddressDeleteSheet
--- src/pages/ProfilePage/AddressesPage/AddressesPage.tsx
+++ src/pages/ProfilePage/AddressesPage/AddressesPage.tsx
@@ -6,10 +6,16 @@ import useAuthStore from "../../../stores/authStore";
 import Card from "../../../components/Card/Card";
 import {Pill} from "@mantine/core";
 import {IconCheck, IconEdit, IconTrashX} from "@tabler/icons-react";
+import BottomActionSheet from "../../../components/BottomActionSheet/BottomActionSheet";
+import AddressDeleteSheet from "../../../components/AddressDeleteSheet/AddressDeleteSheet";
+import {useDisclosure} from "@mantine/hooks";
+import {useState} from "react";
 
 export function AddressesPage() {
 	const userData = useAuthStore(state => state.userData)
+	const [opened, {open, close}] = useDisclosure(false)
 	const {data, isLoading} = useUserAddressListQuery({id: userData.id, token: userData.token})
+	const [selectedAddress, setSelectedAddress] = useState<string>('')
 	return (
 		<section>
 			<BackButton />
@@ -26,13 +32,19 @@ export function AddressesPage() {
 							</div>
 							<div>
 								<Link to={`edit/${addr._id}`}><IconEdit/></Link>
-								<button><IconTrashX/></button>
+								<button onClick={() => {
+									setSelectedAddress(addr._id)
+									open()
+								}}><IconTrashX/></button>
 							</div>
 						</div>
 					</Card>
 				))}
 			</div>
 			<Link to={appRoutes.newAddress}>Dodaj adres</Link>
+			<BottomActionSheet opened={opened} close={close} withCloseButton={false} size={'35%'}>
+				<AddressDeleteSheet close={close} addressId={selectedAddress} token={userData.token}/>
+			</BottomActionSheet>
 		</section>
 	)
 }
