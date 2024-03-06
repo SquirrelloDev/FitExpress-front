401009dced01eb3b59836e9e31e6378fa3a3ee6a
SquirrelloDev <olejnike2000@gmail.com>
2024-03-06 09:49:23 +0100

    Add registering function and the service worker itself

---
+++ src/utils/registerSW.ts
@@ -0,0 +1,15 @@
+let defferedPrompt
+ export async function registerSW(){
+    if( 'serviceWorker' in navigator) {
+        const registered = await navigator.serviceWorker.register('/sw.js');
+        if (!registered) {
+            console.log('SW Registered!')
+        }
+        window.addEventListener('beforeinstallprompt', (e) =>{
+            e.preventDefault()
+            defferedPrompt = e;
+            return false
+        })
+    }
+}
+registerSW()
