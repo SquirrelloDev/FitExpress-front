9e92f45a6f04a2f41841fa1359363c32ac68b835
SquirrelloDev <olejnike2000@gmail.com>
2024-03-09 12:13:02 +0100

    Add error handling to edit form

--- src/pages/OrderManagement/OrderEdit.tsx
+++ src/pages/OrderManagement/OrderEdit.tsx
@@ -4,20 +4,27 @@ import useAuthStore from "../../stores/authStore";
 import {Grid} from "react-loader-spinner";
 import OrderForm from "../../components/OrderManagement/OrderForm";
 import {useUserAddressListQuery} from "../../queries/addresses/listing";
+import {Alert} from "@mantine/core";
+import alertStyles from "../../sass/components/alert.module.scss";
 
 function OrderEdit() {
     const params = useParams()
     const userData = useAuthStore((state) => state.userData)
     const dietId = params.dietId
-    const {data, isLoading} = useOneOrderListQuery({id: dietId!, token: userData.token})
-    const {data: userAddresses, isLoading: isAddressLoading} = useUserAddressListQuery({id: userData.id, token: userData.token})
+    const {data, isLoading, isError, isSuccess, error} = useOneOrderListQuery({id: dietId!, token: userData.token})
+    const {data: userAddresses, isLoading: isAddressLoading, isError: isAddressError, isSuccess: isAddressSuccess} = useUserAddressListQuery({id: userData.id, token: userData.token})
     return (
-        <>
+        <section>
             {(isLoading || isAddressLoading) && <Grid/>}
-            {(!isLoading && !isAddressLoading) && (
+            {(isError || isAddressError) && (
+                <Alert variant={'light'} title={'Wystąpił błąd'} color={'red'} classNames={{root: alertStyles.alert,title: alertStyles.alert__title,message: alertStyles.alert__message}}>
+                    {error?.message}
+                </Alert>
+            )}
+            {(isSuccess && isAddressSuccess) && (
                 <OrderForm order={data!.order} addresses={userAddresses!.addresses} userToken={userData.token}/>
             )}
-        </>
+        </section>
     )
 }
 
